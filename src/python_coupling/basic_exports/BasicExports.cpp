9275b17e90979d9575b4da02eee8189b04e27291
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-04 15:18:20 +0100

    fix deprecation warning in pybind11

--- src/python_coupling/basic_exports/BasicExports.cpp
+++ src/python_coupling/basic_exports/BasicExports.cpp
@@ -44,6 +44,15 @@
 
 #include <functional>
 
+// specialize operator== since == is deprecated in pybind11
+template<>
+bool walberla::domain_decomposition::internal::BlockData::Data< pybind11::object >::operator==(
+   const BlockData::DataBase& rhs) const
+{
+   const Data< pybind11::object >* rhsData = dynamic_cast< const Data< pybind11::object >* >(&rhs);
+   return (rhsData == &rhs) && (data_->is(*(rhsData->data_)));
+}
+
 namespace py = pybind11;
 namespace walberla {
 namespace python_coupling {
