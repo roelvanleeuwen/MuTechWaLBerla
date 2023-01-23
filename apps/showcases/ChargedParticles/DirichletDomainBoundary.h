7eaa22620dd4fc09bd0fdef384d02e5130ea537a
zy69guqi <richard.angersbach@fau.de>
2023-01-23 12:54:52 +0100

    Implement dirichlet function boundary handling class

--- apps/showcases/ChargedParticles/DirichletDomainBoundary.h
+++ apps/showcases/ChargedParticles/DirichletDomainBoundary.h
@@ -110,6 +110,108 @@ void DirichletDomainBoundary< PdeField >::apply(PdeField* p, const CellInterval&
                                           p->get(x, y, z) = real_c(2) * value - p->get(x + cx, y + cy, z + cz);)
 }
 
+template< typename PdeField >
+class DirichletFunctionDomainBoundary
+{
+ public:
+
+   using ApplyFunction = std::function< void (IBlock* block, PdeField* p, const CellInterval& interval, const cell_idx_t cx, const cell_idx_t cy, const cell_idx_t cz) >;
+
+   DirichletFunctionDomainBoundary(StructuredBlockStorage& blocks, const BlockDataID& fieldId)
+      : blocks_(blocks), fieldId_(fieldId)
+   {
+      for (uint_t i = 0; i != stencil::D3Q6::Size; ++i)
+      {
+         includeBoundary_[i] = true;
+
+         applyFunctions_[i] = {};
+      }
+   }
+
+   void includeBoundary(const stencil::Direction& direction) { includeBoundary_[stencil::D3Q6::idx[direction]] = true; }
+   void excludeBoundary(const stencil::Direction& direction)
+   {
+      includeBoundary_[stencil::D3Q6::idx[direction]] = false;
+   }
+
+   void setFunction( const ApplyFunction func ) { for( uint_t i = 0; i != stencil::D3Q6::Size; ++i ) applyFunctions_[i] = func; }
+   void setFunction( const stencil::Direction & direction, const ApplyFunction func ) { applyFunctions_[stencil::D3Q6::idx[direction]] = func; }
+
+   void operator()();
+
+ protected:
+   StructuredBlockStorage& blocks_;
+   BlockDataID fieldId_;
+
+   bool includeBoundary_[stencil::D3Q6::Size];
+
+   // user-defined apply function
+   ApplyFunction applyFunctions_[ stencil::D3Q6::Size ];
+
+}; // class DirichletFunctionDomainBoundary
+
+template< typename PdeField >
+void DirichletFunctionDomainBoundary< PdeField >::operator()()
+{
+   for (auto blockIt = blocks_.begin(); blockIt != blocks_.end(); ++blockIt)
+   {
+      auto * block = static_cast<blockforest::Block*> (&(*blockIt));
+
+      PdeField* p = block->template getData< PdeField >(fieldId_);
+
+      if (applyFunctions_[stencil::D3Q6::idx[stencil::W]] && includeBoundary_[stencil::D3Q6::idx[stencil::W]] && blocks_.atDomainXMinBorder(*block))
+      {
+         applyFunctions_[stencil::D3Q6::idx[stencil::W]](block, p,
+               CellInterval(
+                  cell_idx_t(-1), cell_idx_t(0)                         , cell_idx_t(0),
+                  cell_idx_t(-1), cell_idx_c(p->ySize()) - cell_idx_t(1), cell_idx_c(p->zSize()) - cell_idx_t(1)),
+               cell_idx_t(1), cell_idx_t(0), cell_idx_t(0));
+      }
+      if (applyFunctions_[stencil::D3Q6::idx[stencil::E]] && includeBoundary_[stencil::D3Q6::idx[stencil::E]] && blocks_.atDomainXMaxBorder(*block))
+      {
+         applyFunctions_[stencil::D3Q6::idx[stencil::E]](block, p,
+               CellInterval(
+                  cell_idx_c(p->xSize()), cell_idx_t(0)                         , cell_idx_t(0),
+                  cell_idx_c(p->xSize()), cell_idx_c(p->ySize()) - cell_idx_t(1), cell_idx_c(p->zSize()) - cell_idx_t(1)),
+               cell_idx_t(-1), cell_idx_t(0), cell_idx_t(0));
+      }
+
+      if (applyFunctions_[stencil::D3Q6::idx[stencil::S]] && includeBoundary_[stencil::D3Q6::idx[stencil::S]] && blocks_.atDomainYMinBorder(*block))
+      {
+         applyFunctions_[stencil::D3Q6::idx[stencil::S]](block, p,
+               CellInterval(
+                  cell_idx_t(0)                         , cell_idx_t(-1), cell_idx_t(0),
+                  cell_idx_c(p->xSize()) - cell_idx_t(1), cell_idx_t(-1), cell_idx_c(p->zSize()) - cell_idx_t(1)),
+               cell_idx_t(0), cell_idx_t(1), cell_idx_t(0));
+      }
+      if (applyFunctions_[stencil::D3Q6::idx[stencil::N]] && includeBoundary_[stencil::D3Q6::idx[stencil::N]] && blocks_.atDomainYMaxBorder(*block))
+      {
+         applyFunctions_[stencil::D3Q6::idx[stencil::N]](block, p,
+               CellInterval(
+                  cell_idx_t(0)                         , cell_idx_c(p->ySize()), cell_idx_t(0),
+                  cell_idx_c(p->xSize()) - cell_idx_t(1), cell_idx_c(p->ySize()), cell_idx_c(p->zSize()) - cell_idx_t(1)),
+               cell_idx_t(0), cell_idx_t(-1), cell_idx_t(0));
+      }
+
+      if (applyFunctions_[stencil::D3Q6::idx[stencil::B]] && includeBoundary_[stencil::D3Q6::idx[stencil::B]] && blocks_.atDomainZMinBorder(*block))
+      {
+         applyFunctions_[stencil::D3Q6::idx[stencil::B]](block, p,
+               CellInterval(
+                  cell_idx_t(0)                         , cell_idx_t(0)                         , cell_idx_t(-1),
+                  cell_idx_c(p->xSize()) - cell_idx_t(1), cell_idx_c(p->ySize()) - cell_idx_t(1), cell_idx_t(-1)),
+               cell_idx_t(0), cell_idx_t(0), cell_idx_t(1));
+      }
+      if (applyFunctions_[stencil::D3Q6::idx[stencil::T]] && includeBoundary_[stencil::D3Q6::idx[stencil::T]] && blocks_.atDomainZMaxBorder(*block))
+      {
+         applyFunctions_[stencil::D3Q6::idx[stencil::T]](block, p,
+               CellInterval(
+                  cell_idx_t(0)                         , cell_idx_t(0)                         , cell_idx_c(p->zSize()),
+                  cell_idx_c(p->xSize()) - cell_idx_t(1), cell_idx_c(p->ySize()) - cell_idx_t(1), cell_idx_c(p->zSize())),
+               cell_idx_t(0), cell_idx_t(0), cell_idx_t(-1));
+      }
+   }
+}
+
 } // namespace walberla
 
 #endif // WALBERLA_DIRICHLETDOMAINBOUNDARY_H
