cb6dae2774b6a0214a1c83f99d3076da31774477
zy69guqi <richard.angersbach@fau.de>
2023-02-15 15:33:20 +0100

    Adapt charged particle setup for workstation execution

--- apps/showcases/ChargedParticles/ChargedParticles.cpp
+++ apps/showcases/ChargedParticles/ChargedParticles.cpp
@@ -536,7 +536,7 @@ int main(int argc, char** argv)
       field::addCloneToStorage< ScalarField_T >(blocks, potentialFieldID, "electric potential field (copy)");
 
    auto poissonSolver = PoissonSolver< DAMPED_JACOBI, true > (/* src */ potentialFieldID, /* dst */ potentialFieldCopyID, /* rhs */ chargeDensityFieldID, blocks,
-                                                             uint_c(50000), real_c(1e-8), uint_c(1000));
+                                                             uint_c(1000), real_c(1e-3), uint_c(1000));
 
    /////////////////////////
    // CHARGE FORCE UPDATE //
