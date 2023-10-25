03846c5ac0de0a7a49d223f3b3bdf3b30feed630
Frederik Hennig <frederik.hennig@fau.de>
2023-10-25 17:50:33 +0900

    Added and tested grid generator and nonuniform apps.

---
+++ apps/tutorials/lbm_generated/01_GenerateLbm.py
@@ -0,0 +1,59 @@
+import sympy as sp
+
+from pystencils import fields
+
+from lbmpy import Stencil, LBStencil, Method, LBMConfig, LBMOptimisation, create_lb_collision_rule
+from lbmpy.boundaries import NoSlip, UBB, ExtrapolationOutflow
+
+from pystencils_walberla import CodeGeneration
+from lbmpy_walberla import generate_lbm_package, lbm_boundary_generator
+
+stencil = LBStencil(Stencil.D3Q19)
+streaming_pattern = 'pull'
+omega = sp.Symbol('omega')
+
+#   Create symbolic fields
+pdfs, pdfs_tmp = fields(f"pdfs({stencil.Q}), pdfs_tmp({stencil.Q}): double[3D]", layout='fzyx')
+density_field, velocity_field = fields(f"density, velocity(3) : double[3D]", layout='fzyx')
+macroscopic_fields = {'density': density_field, 'velocity': velocity_field}
+
+
+#   Define parameters of the lattice Boltzmann method
+lbm_config = LBMConfig(stencil=stencil,
+                       streaming_pattern=streaming_pattern,
+                       method=Method.CENTRAL_MOMENT,
+                       relaxation_rate=omega,
+                       compressible=True)
+
+lbm_opt = LBMOptimisation()
+
+#   Create the collision rule in algebraic form
+collision_rule = create_lb_collision_rule(lbm_config=lbm_config, lbm_optimisation=lbm_opt)
+
+lb_method = collision_rule.method
+
+#   Set up boundary condition objects
+
+noslip = lbm_boundary_generator(class_name='NoSlip', flag_uid='NoSlip', boundary_object=NoSlip())
+
+ubb_velocity = sp.symbols(f'boundary_velocity_:{stencil.D}')
+ubb = lbm_boundary_generator(class_name='UBB', flag_uid='UBB', boundary_object=UBB(ubb_velocity))
+
+outflow_east_obj = ExtrapolationOutflow(normal_direction=(1, 0, 0), lb_method=lb_method)
+outflow = lbm_boundary_generator(class_name=f'Outflow{stencil.name}', flag_uid='Outflow',
+                                 boundary_object=outflow_east_obj)
+
+with CodeGeneration() as ctx:
+    #   This call generates:
+    #     1. The Lattice Storage Specification
+    #     2. The LBM Stream/Collide sweep
+    #     3. Code for Population field initialization, and density/velocity output
+    #     4. Boundary condition implementations
+    generate_lbm_package(ctx,
+                         name="GeneratedLbm",
+                         collision_rule=collision_rule,
+                         lbm_config=lbm_config,
+                         lbm_optimisation=lbm_opt,
+                         nonuniform=True,
+                         boundaries=[noslip, ubb, outflow],
+                         macroscopic_fields=macroscopic_fields)
--- apps/tutorials/lbm_generated/03_NonuniformGrid.cpp
+++ apps/tutorials/lbm_generated/03_NonuniformGrid.cpp
@@ -13,7 +13,7 @@
 //  You should have received a copy of the GNU General Public License along
 //  with waLBerla (see COPYING.txt). If not, see <http://www.gnu.org/licenses/>.
 //
-//! \file 01_BasicLBM.cpp
+//! \file 03_NonuniformGridApp.cpp
 //! \author Frederik Hennig <frederik.hennig@fau.de>
 //
 //======================================================================================================================
@@ -30,13 +30,13 @@
 
 #include "vtk/all.h"
 
-#include "lbm_generated/field/AddToStorage.h"
-#include "lbm_generated/field/PdfField.h"
-#include "lbm_generated/communication/UniformGeneratedPdfPackInfo.h"
-
+#include "GeneratedLbmBoundaryCollection.h"
 #include "GeneratedLbmStorageSpecification.h"
 #include "GeneratedLbmSweepCollection.h"
-#include "GeneratedLbmBoundaryCollection.h"
+#include "lbm_generated/communication/NonuniformGeneratedPdfPackInfo.h"
+#include "lbm_generated/field/AddToStorage.h"
+#include "lbm_generated/field/PdfField.h"
+#include "lbm_generated/refinement/BasicRecursiveTimeStep.h"
 
 namespace walberla
 {
@@ -46,7 +46,7 @@ using namespace lbm_generated;
 using LSS_T      = lbm::GeneratedLbmStorageSpecification;
 using PdfField_T = PdfField< LSS_T >;
 
-using Stencil    = LSS_T::Stencil;
+using Stencil              = LSS_T::Stencil;
 using CommunicationStencil = LSS_T::CommunicationStencil;
 
 using VectorField_T = field::GhostLayerField< real_t, Stencil::D >;
@@ -57,17 +57,30 @@ using FlagField_T   = field::FlagField< flag_t >;
 using SweepCollection_T    = lbm::GeneratedLbmSweepCollection;
 using BoundaryCollection_T = lbm::GeneratedLbmBoundaryCollection< FlagField_T >;
 
-using PackInfo_T = UniformGeneratedPdfPackInfo< PdfField_T >;
+using blockforest::communication::NonUniformBufferedScheme;
 
 void main(int argc, char** argv)
 {
-   walberla::Environment env(argc, argv);
+   Environment env(argc, argv);
+   mpi::MPIManager::instance()->useWorldComm();
 
-   auto config = env.config();
+   auto config      = env.config();
+   auto domainSetup = config->getOneBlock("DomainSetup");
+   const std::string blockForestFilestem =
+      domainSetup.getParameter< std::string >("blockForestFilestem", "blockforest");
+   Vector3< uint_t > cellsPerBlock = domainSetup.getParameter< Vector3< uint_t > >("cellsPerBlock");
 
-   auto blocks = blockforest::createUniformBlockGridFromConfig(config);
+   // Load structured block forest from file
+   std::ostringstream oss;
+   oss << blockForestFilestem << ".bfs";
+   const std::string setupBlockForestFilepath = oss.str();
 
-   // LBM Parameters
+   WALBERLA_LOG_INFO_ON_ROOT("Creating structured block forest...")
+   auto bfs    = std::make_shared< BlockForest >(uint_c(MPIManager::instance()->worldRank()),
+                                              setupBlockForestFilepath.c_str(), false);
+   auto blocks = std::make_shared< StructuredBlockForest >(bfs, cellsPerBlock[0], cellsPerBlock[1], cellsPerBlock[2]);
+   blocks->createCellBoundingBoxes();
+
    auto parameters        = config->getOneBlock("Parameters");
    const real_t omega     = parameters.getParameter< real_t >("omega", real_c(1.4));
    const uint_t timesteps = parameters.getParameter< uint_t >("timesteps", uint_c(50));
@@ -88,12 +101,15 @@ void main(int argc, char** argv)
    };
 
    // Field Setup
-   const LSS_T storageSpec      = LSS_T();
-   const BlockDataID pdfFieldID = addPdfFieldToStorage< LSS_T >(blocks, "pdfs", storageSpec, uint_c(1), field::fzyx);
-   const BlockDataID densityFieldId = field::addToStorage< ScalarField_T >(blocks, "density", real_c(1.0), field::fzyx);
+   const LSS_T storageSpec = LSS_T();
+   const BlockDataID pdfFieldID =
+      lbm_generated::addPdfFieldToStorage(blocks, "pdfs", storageSpec, uint_c(2), field::fzyx);
    const BlockDataID velFieldId =
-      field::addToStorage< VectorField_T >(blocks, "vel", real_c(0.0), field::fzyx, 1, false, velInit);
-   const BlockDataID flagFieldID = field::addFlagFieldToStorage< FlagField_T >(blocks, "Boundary Flag Field");
+      field::addToStorage< VectorField_T >(blocks, "vel", real_c(0.0), field::fzyx, uint_c(2));
+   const BlockDataID densityFieldId =
+      field::addToStorage< ScalarField_T >(blocks, "density", real_c(1.0), field::fzyx, uint_c(2));
+   const BlockDataID flagFieldID =
+      field::addFlagFieldToStorage< FlagField_T >(blocks, "Boundary Flag Field", uint_c(3));
 
    // Boundary setup
    const FlagUID fluidFlagUID("Fluid");
@@ -107,13 +123,13 @@ void main(int argc, char** argv)
 
    // Simulation Method Setup
    SweepCollection_T lbmSweeps(blocks, pdfFieldID, densityFieldId, velFieldId, omega);
-   BoundaryCollection_T boundaryHandling(blocks, flagFieldID, pdfFieldID, fluidFlagUID,
-                                         ubbVelocity[0], ubbVelocity[1], ubbVelocity[2]);
+   BoundaryCollection_T boundaryHandling(blocks, flagFieldID, pdfFieldID, fluidFlagUID, ubbVelocity[0], ubbVelocity[1],
+                                         ubbVelocity[2]);
 
-   // MPI Communication Setup
-   auto packInfo = std::make_shared< PackInfo_T >(pdfFieldID);
-   blockforest::communication::UniformBufferedScheme< CommunicationStencil > commScheme(blocks);
-   commScheme.addPackInfo(packInfo);
+   // Communication
+   auto communication = std::make_shared< NonUniformBufferedScheme< CommunicationStencil > >(blocks);
+   auto packInfo      = lbm_generated::setupNonuniformPdfCommunication< PdfField_T >(blocks, pdfFieldID);
+   communication->addPackInfo(packInfo);
 
    // VTK Output
    const uint_t vtkWriteFrequency = parameters.getParameter< uint_t >("vtkWriteFrequency", 10);
@@ -124,8 +140,10 @@ void main(int argc, char** argv)
    vtkOutput->addCellDataWriter(velWriter);
 
    vtkOutput->addBeforeFunction([&]() {
-      for (auto& block : *blocks){
-         lbmSweeps.calculateMacroscopicParameters(&block);}
+      for (auto& block : *blocks)
+      {
+         lbmSweeps.calculateMacroscopicParameters(&block);
+      }
    });
 
    vtkOutput->addCellExclusionFilter(field::FlagFieldCellFilter< FlagField_T >(flagFieldID, FlagUID("NoSlip")));
@@ -139,11 +157,11 @@ void main(int argc, char** argv)
    // Time Loop
    SweepTimeloop loop(blocks->getBlockStorage(), timesteps);
 
-   loop.addFuncBeforeTimeStep(vtk::writeFiles(vtkOutput), "VTK Output");
-   loop.add() << BeforeFunction(commScheme, "Communication")
-              << Sweep(boundaryHandling.getSweep(), "Boundary Handling");
-   loop.add() << Sweep(lbmSweeps.streamCollide(), "LBM Stream/Collide");
+   lbm_generated::BasicRecursiveTimeStep< PdfField_T, SweepCollection_T, BoundaryCollection_T > LBMMeshRefinement(
+      blocks, pdfFieldID, lbmSweeps, boundaryHandling, communication, packInfo);
+   LBMMeshRefinement.addRefinementToTimeLoop(loop);
 
+   loop.addFuncAfterTimeStep(vtk::writeFiles(vtkOutput));
 
    // Time Logging
    auto remainingTimeLoggerFrequency =
@@ -155,7 +173,8 @@ void main(int argc, char** argv)
    }
 
    // Initialization
-   for(auto& block: *blocks){
+   for (auto& block : *blocks)
+   {
       lbmSweeps.initialise(&block);
    }
 
@@ -164,6 +183,7 @@ void main(int argc, char** argv)
 
    // The end.
 }
+
 } // namespace walberla
 
 int main(int argc, char** argv)
